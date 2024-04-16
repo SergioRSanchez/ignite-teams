54f98cee34233e7dcc10af939dfc57d026dcec2c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-04-16 09:58:24 -0300

    feat: created input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,8 @@
-import { Header } from '@components/Header';
-
 import { Container, Content, Icon } from './styles';
+
+import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
+import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function NewGroup() {
@@ -17,8 +18,11 @@ export function NewGroup() {
           subtitle='crie a turma para adicionar as pessoas'
         />
 
+        <Input />
+
         <Button 
           title='Criar turma'
+          style={{ marginTop: 20 }}
         />
       </Content>
 
